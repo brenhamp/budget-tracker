c5153aa23ed181d4702a749f428b8c737e171359
brenhamp <bpozzi@trinity.edu>
2022-03-18 21:15:18 -0500

    fixed service-worker not correctly caching offline transactions

--- Develop/public/js/db.js
+++ Develop/public/js/db.js
@@ -1,9 +1,9 @@
 let db;
-const request = indexedDB.open('budget_tracker', 1);
+const request = indexedDB.open('budgetTracker-', 1);
 
 request.onupgradeneeded = function(event) {
     const db = event.target.result;
-    db.createObjectStore('new_budget', { autoIncrement: true });
+    db.createObjectStore('new_transaction', { autoIncrement: true });
   };
 
 request.onsuccess = function(event) {
@@ -19,17 +19,17 @@ request.onerror = function(event) {
 };
 
 function saveRecord(record) {
-    const transaction = db.transaction(['new_budget'], 'readwrite');
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
 
-    const budgetObjectStore = transaction.objectStore('new_budget');
+    const budgetObjectStore = transaction.objectStore('new_transaction');
 
     budgetObjectStore.add(record);
 }
 
 function uploadBudget() {
-    const transaction = db.transaction(['new_budget'], 'readwrite');
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
 
-    const budgetObjectStore = transaction.objectStore('new_budget');
+    const budgetObjectStore = transaction.objectStore('new_transaction');
 
     const getAll = budgetObjectStore.getAll();
   
@@ -49,8 +49,8 @@ function uploadBudget() {
               throw new Error(serverResponse);
             }
   
-            const transaction = db.transaction(['new_budget'], 'readwrite');
-            const budgetObjectStore = transaction.objectStore('new_budget');
+            const transaction = db.transaction(['new_transaction'], 'readwrite');
+            const budgetObjectStore = transaction.objectStore('new_transaction');
             budgetObjectStore.clear();
           })
           .catch(err => {
